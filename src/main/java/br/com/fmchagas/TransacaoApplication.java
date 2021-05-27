33305859212d1cb45e9587b22258a440ac0e8149
Fernando Chagas <fermaccha@gmail.com>
2021-05-27 18:12:13 -0400

    configurações e atualizaçõs

--- src/main/java/br/com/fmchagas/TransacaoApplication.java
+++ src/main/java/br/com/fmchagas/TransacaoApplication.java
@@ -1,4 +1,4 @@
-package br.com.fmchagas.transacao;
+package br.com.fmchagas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class TransacaoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TransacaoApplication.class, args);
 	}
-
 }
